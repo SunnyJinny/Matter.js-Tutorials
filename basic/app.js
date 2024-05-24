8956b59582f25f1b46e4868757c1bab5a56bc794
Hyejin Kang <aufkang.hj@gmail.com>
2024-05-24 09:53:12 +0200

    feat: create Basic

--- basic/app.js
+++ basic/app.js
@@ -13,8 +13,8 @@ var render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 300,
-    height: 400,
+    width: 800,
+    height: 800,
     wireframes: false,
     background: "indigo",
   },
@@ -23,10 +23,12 @@ var render = Render.create({
 // create two boxes and a ground
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ballA = Bodies.circle(380, 100, 40);
+var ballB = Bodies.circle(420, 20, 50, { render: { fillStyle: "green" } });
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+Composite.add(engine.world, [boxA, boxB, ballA, ballB, ground]);
 
 // run the renderer
 Render.run(render);
