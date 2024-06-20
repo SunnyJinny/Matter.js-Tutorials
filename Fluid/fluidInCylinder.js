3611e21015a56a8e2be58b55c7ddbf2ebc929f44
Hyejin Kang <aufkang.hj@gmail.com>
2024-06-20 11:25:56 +0200

    fix: (completed) implementation fluid flow

--- Fluid/fluidInCylinder.js
+++ Fluid/fluidInCylinder.js
@@ -1,3 +1,13 @@
+const canvas = document.querySelector("#matter-canvas");
+let canvasWidth = innerWidth;
+let canvasHeight = innerHeight;
+
+// const stdDeviation = [8, 10];
+// const colorMatrix = ["15 -3", "30 -5"];
+
+let circles = [];
+let glass;
+
 const Engine = Matter.Engine,
   Render = Matter.Render,
   Runner = Matter.Runner,
@@ -8,21 +18,24 @@ const Engine = Matter.Engine,
   Body = Matter.Body,
   Bodies = Matter.Bodies,
   Composite = Matter.Composite,
-  Composites = Matter.Composites,
-  Svg = Matter.Svg,
-  Vertices = Matter.Vertices;
+  Composites = Matter.Composites;
 
-let engine = Engine.create(),
+let engine = Engine.create({
+    constraintIterations: 10,
+    positionIterations: 10,
+  }),
   world = engine.world;
 
-const render = Render.create({
-  element: document.body,
+let render = Render.create({
+  //element: document.body,
+  canvas: canvas,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    width: canvasWidth,
+    height: canvasHeight,
     wireframes: false,
-    background: "#f4f4f4",
+    background: "transparent",
+    pixelRatio: 1,
   },
 });
 Render.run(render);
@@ -31,107 +44,97 @@ Render.run(render);
 const runner = Runner.create();
 Runner.run(runner, engine);
 
-// 바닥 설정
-// const floor = Bodies.rectangle(400, 580, 810, 60, {
-//   isStatic: true,
-//   render: { fillStyle: "#060a19" },
-// });
-// Composite.add(world, floor);
+// 액체를 circle로 만들어서 표현
+
+function createLiquid() {
+  const x = 50;
+  const y = 350;
+  const radius = randomNumBetween(5, 7);
+  const body = Bodies.circle(x, y, radius, {
+    friction: 0, // 마찰력
+    frictionAir: 0, // 공기 마찰력
+    density: 1, // 밀도
+    restitution: 0.2, // 반발력
+    render: {
+      fillStyle: "#2065A5",
+    },
+  });
+  Body.applyForce(body, body.position, { x: 1, y: 0 });
+  Composite.add(world, body);
+  circles.push(body);
+}
 
 // 실린더 만들어보자
-let glass;
 function Glass() {
-  // const glassImg = document.getElementById("glass");
-  this.cx = 300;
-  this.cy = 200;
-  const thickness = 4;
-
-  const leftDown = Bodies.rectangle(
-    this.cx - 110,
-    this.cy + 50,
-    thickness,
-    50,
-    {
+  const thickness = 30;
+  const radius = 20;
+  const wallColor = "#ff0000";
+  const done = "#00000000";
+  const walls = [
+    Bodies.rectangle(300 - 98, 230 - 29, thickness, 180 + 30, {
+      chamfer: { radius: [0, 0, radius, radius] },
       isStatic: true,
-    }
-  );
-  const leftUp = Bodies.rectangle(this.cx - 110, this.cy - 50, thickness, 100, {
-    isStatic: true,
-  });
-  const rightDown = Bodies.rectangle(
-    this.cx + 10,
-    this.cy + 50,
-    thickness,
-    50,
-    {
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(220 - 100, 320 - 25, 160 + 30, thickness, {
+      chamfer: { radius: [0, 0, radius, 0] },
       isStatic: true,
-    }
-  );
-  const rightUp = Bodies.rectangle(this.cx + 10, this.cy - 50, thickness, 100, {
-    isStatic: true,
-  });
-  const bottom = Bodies.rectangle(this.cx - 50, this.cy + 75, 125, thickness, {
-    isStatic: true,
-  });
-  const leftInA = Bodies.rectangle(this.cx - 148, this.cy, 80, thickness, {
-    isStatic: true,
-  });
-  const leftInB = Bodies.rectangle(this.cx + 48, this.cy + 24, 80, thickness, {
-    isStatic: true,
-  });
-  const rightInA = Bodies.rectangle(this.cx + 48, this.cy, 80, thickness, {
-    isStatic: true,
-  });
-  const rightInB = Bodies.rectangle(
-    this.cx - 148,
-    this.cy + 24,
-    80,
-    thickness,
-    {
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(140 - 100, 350 - 12, thickness, 60 + 56, {
       isStatic: true,
-    }
-  );
-  Composite.add(engine.world, [
-    leftDown,
-    leftUp,
-    rightDown,
-    rightUp,
-    bottom,
-    leftInA,
-    leftInB,
-    rightInA,
-    rightInB,
-  ]);
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(220 - 100, 380, 160 + 30, thickness, {
+      chamfer: { radius: [0, radius, 0, 0] },
+      isStatic: true,
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(300 - 98, 420 + 2, thickness, 80 + 30, {
+      chamfer: { radius: [0, radius, 0, 0] },
+      isStatic: true,
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(400 - 99, 460, 200 + 30, thickness, {
+      isStatic: true,
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(500 - 90, 420 + 2, thickness, 80 + 30, {
+      chamfer: { radius: [radius, 0, 0, 0] },
+      isStatic: true,
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(580 - 90, 380, 160 + 30, thickness, {
+      chamfer: { radius: [radius, 0, 0, 0] },
+      isStatic: true,
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(580 - 90, 320 - 25, 160 + 30, thickness, {
+      chamfer: { radius: [0, 0, 0, radius] },
+      isStatic: true,
+      render: { fillStyle: done },
+    }),
+    Bodies.rectangle(500 - 90, 230 - 29, thickness, 180 + 30, {
+      chamfer: { radius: [0, 0, 0, radius] },
+      isStatic: true,
+      render: { fillStyle: done },
+    }),
+  ];
+  Composite.add(world, walls);
 }
 glass = new Glass();
-// const vertices = [
-//   { x: 116, y: 0.5 },
-//   { x: 116, y: 102.5 },
-//   { x: 0.5, y: 102.5 },
-//   { x: 0.5, y: 140 },
-//   { x: 116, y: 140 },
-//   { x: 116, y: 186.5 },
-//   { x: 264.5, y: 186.5 },
-//   { x: 264.5, y: 140 },
-//   { x: 380, y: 140 },
-//   { x: 380, y: 102.5 },
-//   { x: 264.5, y: 102.5 },
-//   { x: 264.5, y: 0.5 },
-// ];
-
-// const glass = Bodies.fromVertices(300, 200, vertices, {
-//   isStatic: true,
-//   render: {
-//     fillStyle: "#00000000",
-//     strokeStyle: "#000000",
-//     lineWidth: 1,
-//   },
-// });
-
-// 엔진에 바디 추가
-Composite.add(world, glass);
 
 // // 마우스 제약 추가
+// const mouse = Mouse.create(render.canvas);
+// mouse = Mouse.create(canvas);
+
+// mouseConstraint = MouseConstraint.create(engine, {
+//   mouse: mouse,
+//   constraint: {
+//     stiffness: 0.1,
+//     render: { visible: false },
+//   },
+// });
 // const mouse = Mouse.create(render.canvas),
 //   mouseConstraint = MouseConstraint.create(engine, {
 //     mouse: mouse,
@@ -152,3 +155,29 @@ Composite.add(world, glass);
 // function mouseClicked() {
 
 // }
+Events.on(runner, "tick", (e) => {
+  createLiquid();
+  for (let i = circles.length - 1; i >= 0; i--) {
+    if (circles[i].position.y - circles[i].circleRadius > canvasHeight) {
+      Composite.remove(world, circles[i]);
+      circles.splice(i, 1);
+    }
+  }
+});
+
+// function resizeFilter() {
+//   const feGaussianBlur = document.querySelector("#gooey feGaussianBlur");
+//   const feColorMatrix = document.querySelector("#gooey feColorMatrix");
+//   let index;
+//   if (canvasWidth < 600) index = 0;
+//   else index = 1;
+//   feGaussianBlur.setAttribute("stdDeviation", stdDeviation[index]);
+//   feColorMatrix.setAttribute(
+//     "values",
+//     `1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 ${colorMatrix[index]}`
+//   );
+// }
+
+function randomNumBetween(min, max) {
+  return Math.random() * (max - min) + min;
+}
